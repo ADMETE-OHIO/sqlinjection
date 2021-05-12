0f59ff21394466658c6a70b1efed6e410bfa777b
Chad Mourning <chadmourning@gmail.com>
2021-05-12 10:51:10 -0400

    o initial commit, should be a working example of sql injection

---
+++ background.py
@@ -0,0 +1,45 @@
+#background.py>
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(path):
+    connection = None
+    try:
+        connection = sqlite3.connect(path)
+        print("Connection to SQLite DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+        
+def execute_read_query(connection, query):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as e:
+        print(f"The error '{e}' occurred")
+        
+def isSSN(ssn):
+    if(len(ssn)==9):
+       return true
+    return false
+       
+connection = create_connection(".\\test.db")
+
+select_users = "SELECT * from EMPLOYEE"
+users = execute_read_query(connection, select_users)
+
+for user in users:
+    print(user) 
